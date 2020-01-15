719af99a9fd76b70c1cfee424fcd8733d7b8d87a
Daniele Vinci <vinci.dani@gmail.com>
2020-01-15 15:14:11 -0500

    primitive data types Java initialized files

---
+++ ByteShortIntLong/src/academy/learnprogramming/Main.java
@@ -0,0 +1,12 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int myValue=10000;
+	    int myMinValue = Integer.MIN_VALUE;
+	    int myMaxValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinValue);
+        System.out.println("Integer Maximum Value = " + myMaxValue);
+    }
+}
