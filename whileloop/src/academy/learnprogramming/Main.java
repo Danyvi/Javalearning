903935e9a901b8942665cb42dd0283ab5ae79530
Daniele Vinci <vinci.dani@gmail.com>
2020-01-21 22:44:16 -0500

    control flow statements

---
+++ whileloop/src/academy/learnprogramming/Main.java
@@ -0,0 +1,12 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int count =1;
+        while (count !=5) {
+            System.out.println("count value is "+count);
+            count ++;
+        }
+    }
+}
